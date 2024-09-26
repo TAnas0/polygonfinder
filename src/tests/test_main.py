0a394b8d866685a158cc93a62122fd12e64d52c4
TAnas0 <anastiour@gmail.com>
2024-09-27 00:40:32 +0100

    test: add test for creating a provider

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,6 +1,9 @@
 from fastapi.testclient import TestClient
 from src.main import app
+import src.tests.utils
 
+
+setup_db = src.tests.utils.setup_db
 
 client = TestClient(app)
 
@@ -9,3 +12,20 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the PolygonFinder API"}
+
+
+# Test creating a provider
+def test_create_provider(setup_db):
+    provider_data = {
+        "name": "Test Provider",
+        "email": "test@gmail.com",
+        "phone_number": "+212634567890",
+        "language": "en",
+        "currency": "USD",
+    }
+
+    response = client.post("/api/v1/providers/", json=provider_data)
+
+    assert response.status_code == 200
+    assert response.json()["name"] == provider_data["name"]
+    assert response.json()["email"] == provider_data["email"]
