e4d1a996ed57cb36b37cafca166f9efa477ed070
TAnas0 <anastiour@gmail.com>
2024-09-26 22:40:30 +0100

    feat(schemas): define schemas for creating, updating and fetching a Provider

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 
 # Schema for Provider
@@ -8,3 +9,22 @@ class ProviderBase(BaseModel):
     phone_number: str
     language: str
     currency: str
+
+
+class ProviderCreate(ProviderBase):
+    pass
+
+
+class ProviderUpdate(ProviderBase):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    phone_number: Optional[str] = None
+    language: Optional[str] = None
+    currency: Optional[str] = None
+
+
+class Provider(ProviderBase):
+    id: int
+
+    class Config:
+        orm_mode = True
