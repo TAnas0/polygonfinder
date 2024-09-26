4436e40f4af676e50d0f756069be0fa8d2919f4a
TAnas0 <anastiour@gmail.com>
2024-09-26 18:43:55 +0100

    feat(alembic): set DB URL from environment variables and target metadata to Base model

--- alembic/env.py
+++ alembic/env.py
@@ -2,12 +2,18 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from src.database import DATABASE_URL
+from src.models import Base
+
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+# Set the URL in the Alembic config
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -17,8 +23,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = DATABASE_URL
     context.configure(
         url=url,
         target_metadata=target_metadata,
