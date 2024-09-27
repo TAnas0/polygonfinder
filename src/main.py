d6fab8fc1fcc584131ac6c35882d18b4bcf2edcf
TAnas0 <anastiour@gmail.com>
2024-09-27 16:21:54 +0100

    feat(main): rate limit the API to 5 calls per minute max

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from .routes import router
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
+
+# Create a Limiter instance
+limiter = Limiter(key_func=get_remote_address, default_limits=["5/minute"])
 app = FastAPI()
+
+# Register the Limiter middleware
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 app.include_router(router, prefix="/api/v1")
 
