523dda6876a450a1115140c177558b807e8d81ff
TAnas0 <anastiour@gmail.com>
2024-09-27 11:56:27 +0100

    fix(main): remove creation of tables outside of Alembic

--- src/main.py
+++ src/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
-from .database import engine
-from .models import Base
 from .routes import router
 
 app = FastAPI()
 
 app.include_router(router, prefix="/api/v1")
-
-
-# Create the database tables
-Base.metadata.create_all(bind=engine)  # TODO replace with Alembic migrations
 
 
 @app.get("/")
